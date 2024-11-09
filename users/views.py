864d342d8b54f5d6c560a366b4423ec788355bb0
Oracle <122623090+mateo444-star@users.noreply.github.com>
2024-11-09 17:42:07 +0700

    Commit #1

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Home_page')
+
+def products(request):
+    return HttpResponse('Products_page')
+
+def customer(request):
+    return HttpResponse('Customer_page')
